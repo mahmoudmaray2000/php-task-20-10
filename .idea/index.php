6d7a6e077903c00ad50e2761737e6b077d863324
mahmoudmaray <mahmoudmaray13@gmail.com>
2023-10-20 20:38:53 +0300

    first commit

---
+++ .idea/index.php
@@ -0,0 +1,166 @@
+// 1 //
+// function checkEvenOrOdd($number) {
+//     if($number % 2 == 0) {
+//         echo "$number is even.";
+//     } else {
+//         echo "$number is odd.";
+//     }
+// }
+
+// checkEvenOrOdd(4); // Output: 4 is even.
+// checkEvenOrOdd(7);
+
+// 2//
+
+// function fizzBuzz($n) {
+//     for ($i = 1; $i <= $n; $i++) {
+//         if ($i % 3 == 0 && $i % 5 == 0) {
+//             echo "FizzBuzz ";
+//         } elseif ($i % 3 == 0) {
+//             echo "Fizz ";
+//         } elseif ($i % 5 == 0) {
+//             echo "Buzz ";
+//         } else {
+//             echo "$i ";
+//         }
+//     }
+// }
+
+// fizzBuzz(100);
+
+
+// 3 //
+
+
+// function reverseString($str) {
+//     $length = strlen($str);
+//     $reversed = '';
+
+//     for ($i = $length - 1; $i >= 0; $i--) {
+//         $reversed .= $str[$i];
+//     }
+
+//     return $reversed;
+// }
+
+
+// $originalString = "welcome";
+// $reversedString = reverseString($originalString);
+
+// echo "Original string: $originalString\n";
+// echo "Reversed string: $reversedString\n";
+
+// 4 //
+
+// function calculateCircleArea($radius) {
+//     $area = M_PI * pow($radius, 2);
+//     return $area;
+// }
+
+// function calculateCircleCircumference($radius) {
+//     $circumference = 2 * M_PI * $radius;
+//     return $circumference;
+// }
+
+// // Test case with radius = 5
+// $radius = 5;
+// $area = calculateCircleArea($radius);
+// $circumference = calculateCircleCircumference($radius);
+
+// echo "Radius: $radius\n";
+// echo "Area: $area\n";
+// echo "Circumference: $circumference\n";
+
+ // 5 //
+
+
+// function checkIfSumOrOneIs50($num1, $num2) {
+//     if ($num1 == 50 || $num2 == 50 || $num1 + $num2 == 50) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+
+// $result1 = checkIfSumOrOneIs50(25, 50); // Returns true
+// $result2 = checkIfSumOrOneIs50(30, 20); // Returns true
+// $result3 = checkIfSumOrOneIs50(10, 15); // Returns false
+
+// var_dump($result1);
+// var_dump($result2);
+// var_dump($result3);
+
+// 6 //
+// function checkPositiveAndNegative($num1, $num2) {
+//     if (($num1 > 0 && $num2 < 0) || ($num1 < 0 && $num2 > 0)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+
+// $result1 = checkPositiveAndNegative(5, -10); // Returns true
+// $result2 = checkPositiveAndNegative(-8, 3); // Returns true
+// $result3 = checkPositiveAndNegative(7, 8); // Returns false
+
+// var_dump($result1);
+// var_dump($result2);
+// var_dump($result3);
+
+// 7 //
+
+// function checkMultiples($number) {
+//     if ($number % 5 == 0 || $number % 8 == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// // Test cases
+// $result1 = checkMultiples(10); // Returns true (multiple of 5)
+// $result2 = checkMultiples(16); // Returns true (multiple of 8)
+// $result3 = checkMultiples(7);  // Returns false (not a multiple of 5 or 8)
+
+// var_dump($result1);
+// var_dump($result2);
+// var_dump($result3);
+
+// 8 //
+// function findLargest($num1, $num2, $num3) {
+//     $largest = $num1;
+
+//     if ($num2 > $largest) {
+//         $largest = $num2;
+//     }
+
+//     if ($num3 > $largest) {
+//         $largest = $num3;
+//     }
+
+//     return $largest;
+// }
+
+// // Test case
+// $largestNumber = findLargest(15, 30, 10);
+// echo "The largest number is: $largestNumber";
+
+// 9 //
+// $sum = 0;
+
+// for ($i = 1; $i <= 10; $i++) {
+//     $sum += $i;
+// }
+
+// echo "The sum of numbers from 1 to 10 is: $sum";
+
+// 10  //
+
+// for ($i = 1; $i <= 5; $i++) {
+//     for ($j = 1; $j <= $i; $j++) {
+//         echo '*';
+//     }
+//     echo "\n";
+// }
